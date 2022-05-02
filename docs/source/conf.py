29a2f2942ce490bf9511cce5a28c6abe42e2bd4d
Morty Space <morty.space@gmail.com>
2022-05-02 07:52:04 +0200

    Updated docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ from cryptocom.exchange import __version__
 # -- Project information -----------------------------------------------------
 
 project = "cryptocom-exchange"
-copyright = "2020-2021"
+copyright = "2020-2022"
 author = "Yaroslav Rudenok [MortySpace]"
 
 # The short X.Y version
