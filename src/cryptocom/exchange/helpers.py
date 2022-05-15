0756bb67580b35ed9c7dd1d0e92d91fdd09cc784
Morty Space <morty.space@gmail.com>
2022-05-15 13:35:48 +0200

    doc: Added docstrings for roudnings

--- src/cryptocom/exchange/helpers.py
+++ src/cryptocom/exchange/helpers.py
@@ -1,4 +1,5 @@
 def round_up(value: float, precision: int) -> float:
+    """Rounds number to upper precision 0.13 -> 0.2"""
     pwr = 10**precision
     rounded = round((value * pwr) / pwr, precision)
     if rounded < value:
@@ -7,5 +8,6 @@ def round_up(value: float, precision: int) -> float:
 
 
 def round_down(value: float, precision: int) -> float:
+    """Rounds number to lower precision 0.13 -> 0.1"""
     pwr = 10**precision
     return round((value * pwr) / pwr, precision)
