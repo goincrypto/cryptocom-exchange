346f3852c88f9ceb6403dd45c1d1492c42abb234
Morty Space <morty.space@gmail.com>
2024-10-11 23:50:35 +0200

    fix: [round] Major issue with rounding in some cases fixed with math module

--- src/cryptocom/exchange/helpers.py
+++ src/cryptocom/exchange/helpers.py
@@ -1,18 +1,16 @@
+import math
 import typing as TP
 
 
 def round_up(value: TP.Union[float, str], precision: int) -> float:
     """Rounds number to upper precision 0.13 -> 0.2"""
-    pwr = 10**precision
     value = float(value)
-    rounded = round((value * pwr) / pwr, precision)
-    if rounded < value:
-        rounded = round((value * pwr + 1) / pwr, precision)
-    return rounded
+    pwr = 10**precision
+    return math.ceil(value * pwr) / pwr
 
 
 def round_down(value: TP.Union[float, str], precision: int) -> float:
     """Rounds number to lower precision 0.13 -> 0.1"""
     value = float(value)
     pwr = 10**precision
-    return round((value * pwr) / pwr, precision)
+    return math.floor(value * pwr) / pwr
